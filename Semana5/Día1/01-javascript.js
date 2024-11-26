fa06450aea948f5fb376a5e1f13163ecd68b1805
Jorge Montesinos <osmarmontes50@gmail.com>
2024-11-25 20:08:12 -0500

    avance

---
+++ Semana5/Día1/01-javascript.js
@@ -0,0 +1,25 @@
+console.log("Hola Mundo!");
+
+//Queremos definir la información 
+//VARIABLES
+//NO RECOMIENDO QUE UTILICEN VAR
+//vamos a estar utilizando más que todo let
+let primerNombre = "Edmundo";
+
+console.log(primerNombre);
+
+// var primerNombre = "Edwin";
+// console.log(primerNombre);
+
+//String, le indicamos que es una cadena de carácteres
+let apellido = "FuentesRivera";
+
+//number, e indicamos directamente el número
+let edad = 50;
+
+console.log(edad);
+
+//cambio de valor
+edad = 60;
+
+console.log(edad);
