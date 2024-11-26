d0c9e520d4b9f6e0ff8cd368708d878f42bff03f
Jorge Montesinos <osmarmontes50@gmail.com>
2024-11-25 21:36:03 -0500

    avance

---
+++ Semana5/Día1/02-operaciones.js
@@ -0,0 +1,18 @@
+let procesador = 300;
+let memoria = 200;
+let placaMadre = 150;
+
+/*  comentario de bloque
+Las operaciones aritméticas básicas son
+intuitivas, ya que usan los mismos símbolos(operadores) ,
+que usariamos nosotros en general + -  *  / 
+*/
+
+//comentario de una sola línea
+let precioTotal = procesador + memoria + placaMadre;
+console.log(precioTotal);
+
+// resta
+let descuentoFijo = 75;
+let precioDescontado = precioTotal - descuentoFijo;
+console.log(precioDescontado);
