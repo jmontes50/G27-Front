10e86c7c3b83bf0ccda2c28ca9f4db945dd83631
Jorge Montesinos <osmarmontes50@gmail.com>
2024-11-25 22:58:40 -0500

    avance

--- Semana5/Día1/04-prompt.js
+++ Semana5/Día1/04-prompt.js
@@ -6,7 +6,7 @@ console.log(ticket);
 
 //todo lo que se obtiene desde el navegador suele ser un texto (string)
 
-console.log(typeof ticket)
+console.log(typeof ticket);
 
 let ticketNumber = parseInt(ticket);
 
---
+++ Semana5/Día1/05-ejercicio.js
@@ -0,0 +1,10 @@
+let alto = prompt("Çúal es la altura");
+let ancho = prompt("Cúal es el ancho");
+
+//number
+let altoConvertido = parseFloat(alto);
+let anchoConverido = parseFloat(ancho);
+
+let area = altoConvertido * anchoConverido / 2;
+
+console.log(area);
