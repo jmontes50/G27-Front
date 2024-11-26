49f007da133d4bcbf4d7218c071c8fc143def263
Jorge Montesinos <osmarmontes50@gmail.com>
2024-11-25 22:07:01 -0500

    avance

--- Semana5/Día1/02-operaciones.js
+++ Semana5/Día1/02-operaciones.js
@@ -17,9 +17,39 @@ let descuentoFijo = 75;
 let precioDescontado = precioTotal - descuentoFijo;
 console.log("El precio ya con dscto es:", precioDescontado);
 
+// multiplicación
 let precioPorDos = precioDescontado * 2;
 console.log("Este es el precio por 02 unid.:", precioPorDos);
 
 let precioConIGV = precioPorDos * 1.17;
-console.log("precio con impuestos", precioConIGV);
+console.log("Precio con impuestos", precioConIGV);
 
+// división
+let enCuotas = precioConIGV / 4;
+console.log("Cuota individual", enCuotas);
+
+
+//
+let a = 10;
+
+console.log("A valor inicial", a);
+//de esta manera se indica
+a = a + 1; //11
+
+a++; //12
+console.log("sumando + 1:", a);
+
+a--; //11
+console.log("restando - 1:",a);
+
+// const body = document.querySelector("body");
+
+// body.innerHTML = a;
+//estamos indicando que a se sume +100
+// a = a + 100;
+a+=100;
+console.log(a);
+
+// a = a - 6;
+a-=6;
+console.log(a);
