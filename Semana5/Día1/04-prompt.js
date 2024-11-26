cd75c684db8a43af00f550d47256cc43d85af79a
Jorge Montesinos <osmarmontes50@gmail.com>
2024-11-25 22:47:36 -0500

    avance

--- Semana5/Día1/04-prompt.js
+++ Semana5/Día1/04-prompt.js
@@ -3,3 +3,11 @@
 let ticket = prompt("Ingrese su número");
 
 console.log(ticket);
+
+//todo lo que se obtiene desde el navegador suele ser un texto (string)
+
+console.log(typeof ticket)
+
+let ticketNumber = parseInt(ticket);
+
+console.log(ticketNumber);
