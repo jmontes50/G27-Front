5adea356abe8aec2cc732360f8b66d4682bfbc9f
Jorge Montesinos <osmarmontes50@gmail.com>
2024-11-27 19:42:34 -0500

    avance

--- Semana5/Día3/01-elseif.js
+++ Semana5/Día3/01-elseif.js
@@ -7,3 +7,29 @@ if(!estaLogueado) {
 } else {
     console.log("Ya ingresaste, bienvenido de vuelta");
 }
+
+//else if sintaxis
+let num = 4;
+/**
+ * if(expresion){
+ * 
+ * }else if(expresion){
+ * 
+ * } else {
+ * 
+ * }
+ */
+if (num > 8) {
+  console.log("Es mayor que 8");
+} else if (num > 5) {
+  console.log("Es mayor que 5");
+} else if(num > 3){
+  console.log("Es mayor que 3");
+} else {
+  console.log("Debe ser mayor que 0");
+} 
+
+//else if
+let correo = "usuario@gmail.com";
+let contrasena = "";
+let aceptoTerminos = false;
