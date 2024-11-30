3629dec31ccb801a3aea65d2891410602da6986b
Jorge Montesinos <osmarmontes50@gmail.com>
2024-11-29 22:05:41 -0500

    avance

--- Semana5/Día5/03-for.js
+++ Semana5/Día5/03-for.js
@@ -2,6 +2,18 @@
 // codigo a ejecutar repetitivamente hasta que la expresión sea falsa
 //}
 
+/*
 for (let i = 0; i < 10; i++) {
     console.log(`Mostrando el # ${i}`);
 }
+
+*/
+
+let numero = +prompt("Ingrese un número para generar tabla");
+
+for(let i = 0; i <= 10; i++) {
+    let resultado = numero * i;
+    console.log(`${numero} X ${i} = ${resultado}`);
+}
+
+console.log("------------------------")
