10372e4c19f1eef1cc33a92bebf928afc9dc1b38
Jorge Montesinos <osmarmontes50@gmail.com>
2024-11-29 22:17:38 -0500

    avance

--- Semana5/Día5/03-for.js
+++ Semana5/Día5/03-for.js
@@ -9,11 +9,27 @@ for (let i = 0; i < 10; i++) {
 
 */
 
+/*
+//solicitar un número
 let numero = +prompt("Ingrese un número para generar tabla");
+
 
 for(let i = 0; i <= 10; i++) {
     let resultado = numero * i;
     console.log(`${numero} X ${i} = ${resultado}`);
 }
 
-console.log("------------------------")
+console.log("------------------------");
+*/
+
+//control de cuentas
+let cantCompras = +prompt("Cuantas compras hiciste?");
+let costoTotal = 0;
+
+for(let i = 1; i <= cantCompras; i++) {
+    let costoCompraActual = +prompt("Cuanto costo está compra?");
+    console.log(`La compro actual costo ${costoCompraActual}`);
+    costoTotal = costoTotal + costoCompraActual;
+}
+
+console.log(`Gastaste un total de ${costoTotal}`);
