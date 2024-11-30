535bc4358b8174ce14a6a6e669847e9de883844d
Jorge Montesinos <osmarmontes50@gmail.com>
2024-11-29 20:42:15 -0500

    avance

--- Semana5/Día5/01-while.js
+++ Semana5/Día5/01-while.js
@@ -1,5 +1,6 @@
 //toda estructura repetitiva necesita un limite
 
+/*
 let limite = 10;
 let inicio = 1;
 //while(expresión que tiene que ser verdadera)
@@ -14,3 +15,16 @@ while (inicio <= limite){
 
 //y cuando el bucle/repetición termine, recién se ejecutará el código en la parte inferior
 console.log("Yo estoy después del while!");
+*/
+
+//solicitamos un número y la entrada la transformamos a number
+let mes = +prompt("Ingrese el número del mes");
+
+//damos las condiciones pero negamos para que pregunte en caso no cumpla que sea entre 1 y 12
+while(!(mes >= 1 && mes <= 12)) {
+    //si no es valido preguntaremos de nuevo
+    mes = +prompt('Número inválido, pruebe de nuevo');
+}
+
+//imprimimos el resultado
+console.log (`Mes ${mes} válido!`);
