346e9ab0f74310e6797abe015cba06b728f4ce36
Jorge Montesinos <osmarmontes50@gmail.com>
2024-12-02 19:46:00 -0500

    avance

---
+++ Semana6/Día1/02-pushpop.js
@@ -0,0 +1,19 @@
+let productos = [
+    "Audifonos",
+    "Micrófono",
+    "Mouse",
+    "USB"
+];
+
+//método(herramienta) -> acción
+console.log("1. ", productos);
+
+//pop remover un item del arr, del final
+productos.pop();
+
+console.log("2. ", productos);
+
+//push, agregar un item al final
+productos.push("Teclado");
+
+console.log("3. ", productos);
