0b5bd496103c3bb6f3a3554139307bffaa2d9854
Jorge Montesinos <osmarmontes50@gmail.com>
2024-12-02 20:21:26 -0500

    avance

--- Semana6/Día1/03-demoarrays.js
+++ Semana6/Día1/03-demoarrays.js
@@ -10,14 +10,23 @@ function listarPlatillos() {
   }
 }
 
+function eliminarPlatilloFinal(){
+  console.log("ELIMINANDO FINAL");
+  platillos.pop();
+  listarPlatillos();
+}
+
 //ejecución
 // listarPlatillos();
 
 let opcion = prompt(`
   ELIJA UNA OPCIÓN
   1. Listar Platillos
+  2. Eliminar Platillo final
   0. Salir`);
 
 if(opcion === "1"){
   listarPlatillos();
-};
+}else if(opcion === "2"){
+  eliminarPlatilloFinal();
+}
