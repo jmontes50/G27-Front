ae4d0c4c21c001ba6c861b1c7dedb39e24a63926
Jorge Montesinos <osmarmontes50@gmail.com>
2024-12-03 19:48:12 -0500

    avance

---
+++ Semana6/Día2/01-ejercicios.js
@@ -0,0 +1,22 @@
+/**
+ * Con un for imprime en la consola los primeros 10 números pares de la tabla de multiplicar del 7.
+ */
+
+/* que es un número par, n % 2 === 0 */
+/* necesitamos un contador */
+/* debemos tener 10 números pares con las condiciones previas */
+
+let contador = 0;
+let indice = 1;
+
+while (contador < 10) {
+    let multiplicacion = indice * 7;
+    //verificando si es par mediante el residuo
+    if(multiplicacion % 2 === 0) {
+        //si es par
+        console.log(multiplicacion);
+        // contador = contador + 1;
+        contador++;
+    }
+    indice++; //1*7 , 2*7, 3*7
+}
