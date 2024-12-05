58874b2d79c1d2489c67af8fad156fe4d87f1ca9
Jorge Montesinos <osmarmontes50@gmail.com>
2024-12-04 19:29:34 -0500

    avance

---
+++ Semana6/Día3/01-mas-objetos.js
@@ -0,0 +1,21 @@
+let artista = {
+  nombre: "Los Prisioneros",
+  origen: "Chile",
+  genero: ["Rock", "Pop", "Punk"],
+  anioInicio: 1983,
+  anioFinal: 2006
+}
+
+console.log(artista);
+
+//acceder al valor de una propiedad con dot notation
+console.log("Nombre: ", artista.nombre);
+//acceder al valor usando bracket notation
+//nombreObjeto["nombrePropiedad"]
+console.log("Origen: ", artista["origen"]);
+//desestructuración, conocer las propiedades de un objeto y en vez de referenciar al objeto indicamos que propiedades tiene:
+//comunmente artista.anioInicio artista.anioFinal
+// let { propiedadesDelObjeto } = nombreObjeto;
+//como estar declarando variables con nombres de propiedades de un objeto
+let { anioInicio, anioFinal } = artista;
+console.log("Año Inicio:", anioInicio);
