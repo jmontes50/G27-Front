b88095e376a5ac25ae8c9dbdca733ee1f823bd83
Jorge Montesinos <osmarmontes50@gmail.com>
2024-12-04 22:01:42 -0500

    avance

--- Semana6/Día3/03-arreglosconobjetos.js
+++ Semana6/Día3/03-arreglosconobjetos.js
@@ -16,6 +16,16 @@ let productos = [
 console.log(productos[0].nombre);
 //for(inicializador; límite; actualizador)
 //i que es 0 se actualizara en cada iteración, a 1, 2, 3
+/*
 for(let i = 0; i < productos.length; i++){
-    console.log(productos[i]);
+    console.log(productos[i].nombre);
 }
+*/
+//obtener una suma de los precios
+let sumaProductos = 0;
+for(let i = 0; i < productos.length; i++){
+    //cada vez que recorramos un producto del arreglo
+    //tomaremos su precio y lo iremos añadiendo a sumaProductos
+    sumaProductos = sumaProductos + productos[i].precio;
+}
+console.log(`La sumatoria de todos los productos es: ${sumaProductos}`);
