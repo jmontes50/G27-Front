bcbba62a3af90a71c8233fb59306d297b8892047
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-11 22:28:37 -0500

    avance

--- Semana7/Día3/03-inventario.js
+++ Semana7/Día3/03-inventario.js
@@ -10,7 +10,7 @@ let inventario = {
      * }
      */
   ],
-  agregarProducto: function(nombre, precio, cantidad){
+  agregarProducto: function(nombre, precio, cantidad, active = true){
     let nuevoProducto = {
       //estamos obteniendo la longitud de los productos para usarlo y dar un id nuevo en base a la cantidad
       id: this.productos.length + 1,
@@ -18,14 +18,28 @@ let inventario = {
       nombre,
       precio,
       cantidad,
-      active: true
+      active
     }
     console.log(nuevoProducto);
     //push para agregarlo a la propiedad productos de inventario
     this.productos.push(nuevoProducto);
+  },
+  actualizarCantidad: function(nombre, cantidad) {
+    let indice = this.encontrarProductoPorNombre(nombre);
+  },
+  encontrarProductoPorNombre: function(nombre){
+    //findIndex me da -1 si es que no encuentro nada y 0 ó + en caso encuentre la posición de lo que estemos buscando
+    let posicion = this.productos.findIndex(function(item) {
+      return item.nombre === nombre
+    })
+    return posicion;
   }
 }
 
 inventario.agregarProducto("Chocolate", 5, 20);
+inventario.agregarProducto("Pavos", 100, 10, false);
 
 console.table(inventario.productos);
+
+let a = inventario.encontrarProductoPorNombre("Chocolate");
+console.log({ a });
