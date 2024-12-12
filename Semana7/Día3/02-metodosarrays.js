8f1682a957d3d8fec18c0fc67201dd4ecc66f925
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-11 20:19:23 -0500

    avance

--- Semana7/Día3/02-metodosarrays.js
+++ Semana7/Día3/02-metodosarrays.js
@@ -1,7 +1,7 @@
 let frutas = ["Kiwi", "Naranjas", "Fresas", "Sandía"];
 
 //herramientas para trabajar con arreglos
-
+console.log("///////////////// FOREACH //////////////////")
 //forEach va a recibir un parámetro, ese parámetro sera una función
 //la función que recibe como parámetro en forEach pude tener más cosas
 //puede tener el item, el indice y el arreglo original
@@ -14,4 +14,14 @@ let forEachFrutas = frutas.forEach(function (fru, indice, arreglo) {
     return fru.toUpperCase();
 });
 
-console.log({ forEachFrutas });
+// console.log({ forEachFrutas });
+console.log("///////////////// MAP //////////////////")
+
+//map a comparación del forEach si me retorna algo
+let frutasEnMayus = frutas.map(function(fru, indice, arreglo){
+    // console.log(fru);
+    //convertir un texto a Mayúsculas
+    return `${indice} - ${fru.toUpperCase()}`;
+});
+
+console.log({ frutasEnMayus });
