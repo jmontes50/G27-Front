80ce640d30692152dd522aaf429f8e40dbc7de63
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-12 22:19:39 -0500

    avance

---
+++ Semana7/Día4/07.1-modulo.js
@@ -0,0 +1,9 @@
+//cuando lo importemos lo podemos llamar como deseemos
+// import esPar from "./07.2-modulo-secundario.js";
+import isEven from "./07.2-modulo-secundario.js";
+
+
+// const resultado = esPar(11);
+const resultado = isEven(10);
+
+console.log(resultado);
---
+++ Semana7/Día4/07.2-modulo-secundario.js
@@ -0,0 +1,6 @@
+const esPar = (num) => {
+    //verificamos si el número es par con base al residuo
+    return num % 2 === 0;
+}
+
+export default esPar;
