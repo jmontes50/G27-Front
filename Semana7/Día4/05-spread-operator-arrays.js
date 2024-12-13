eb356e2e20ab757657c6e0241a0e7b74a6745ed6
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-12 21:37:39 -0500

    avance

---
+++ Semana7/Día4/05-spread-operator-arrays.js
@@ -0,0 +1,15 @@
+let animales = ["Gato", "Perro", "Paloma"];
+
+//porque es una copía por referencía
+let animales2 = animales;
+
+animales2.push("Ratón");
+
+console.log("animales", animales);
+
+console.log("animales2", animales2);
+
+//copia con spread operator, al hacer la copia podemos agregar nuevos items al arreglo
+let animales3 = [...animales, "Pavo", "Cerdo"];
+
+console.log("animales 3", animales3);
