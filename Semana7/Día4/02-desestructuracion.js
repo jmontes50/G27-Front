084d1e28b53379a32c87161dde4f98fdabdebc15
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-12 20:20:22 -0500

    avance

--- Semana7/Día4/02-desestructuracion.js
+++ Semana7/Día4/02-desestructuracion.js
@@ -1,3 +1,8 @@
+//cuando son varios parámetros y hay probabidad de error humano podemos desestructurar los parámetros y enviar un objeto
+function matricula({ nombre, telefono, direccion }) {
+    console.log(`${nombre} con teléfono ${telefono} y dirección ${direccion}, se matrículo`);
+}
+
 let persona = {
     nombre: "Sabrina",
     edad:30,
@@ -10,7 +15,10 @@ let persona = {
 // const edad = persona.edad;
 
 //desestructuración de objetos
-const { nombre, edad } = persona;
+const { nombre, edad, telefono, direccion } = persona;
 
 console.log(nombre);
 console.log(edad);
+
+// matricula(nombre, direccion, telefono);
+matricula(persona)
