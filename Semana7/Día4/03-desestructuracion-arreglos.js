125d1428cdef4ad14fb9a0e3b5bd23c42580e041
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-12 20:30:01 -0500

    avance

---
+++ Semana7/Día4/03-desestructuracion-arreglos.js
@@ -0,0 +1,9 @@
+const frutas = ["Aguaymanto", "Pitahaya", "Tuna"];
+
+// const fruta1 = frutas[0];
+//aplicando desestructuración
+const [fruta1, fruta2, fruta3] = frutas;
+
+console.log(fruta1);
+console.log(fruta2);
+console.log(fruta3);
