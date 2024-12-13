e2c6990656ae99d4cafbf2dc1f5bb2d09abd605c
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-12 22:50:57 -0500

    avance

---
+++ Semana7/Día4/tienda/data.js
@@ -0,0 +1,38 @@
+//información de los productos
+const productos = [
+  {
+    id: 1,
+    nombre: "Laptop Gamer",
+    descripcionCorta:
+      "Laptop con procesador Intel i7 y tarjeta gráfica RTX 3060.",
+    precio: 1500.0,
+  },
+  {
+    id: 2,
+    nombre: "Smartphone",
+    descripcionCorta: "Teléfono con pantalla AMOLED y 128GB de almacenamiento.",
+    precio: 800.0,
+  },
+  {
+    id: 3,
+    nombre: "Auriculares Bluetooth",
+    descripcionCorta:
+      "Auriculares con cancelación de ruido y batería de 20 horas.",
+    precio: 120.0,
+  },
+  {
+    id: 4,
+    nombre: "Smartwatch",
+    descripcionCorta:
+      "Reloj inteligente con monitoreo de salud y GPS integrado.",
+    precio: 250.0,
+  },
+  {
+    id: 5,
+    nombre: "Teclado Mecánico",
+    descripcionCorta: "Teclado con switches táctiles y retroiluminación RGB.",
+    precio: 100.0,
+  },
+];
+
+export default productos;
---
+++ Semana7/Día4/tienda/script.js
@@ -0,0 +1,26 @@
+import productos from "./data.js";
+
+console.log(productos);
+
+const tienda = {
+  productos: [...productos],
+  agregarProducto: function(producto){
+    let nuevoProducto = {
+      ...producto,
+      id: this.productos.length + 1
+    }
+    this.productos.push(nuevoProducto);
+  }
+};
+
+let producto = {
+  nombre: "Kit de limpieza",
+  descripcionCorta: "lorem",
+  precio: 10,
+}
+
+tienda.agregarProducto(producto);
+
+console.log(producto);
+
+console.table(tienda);
