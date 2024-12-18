c2f0978456fccc658ff7cd31f63a7de7afb7dfff
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-17 20:52:45 -0500

    avance

---
+++ Semana8/Día1/02-eventos.js
@@ -0,0 +1,10 @@
+const btnSimple = document.getElementById("botonSimple");
+
+//element.addEventListener("nombreEvento", funciónAEjecutar)
+// btnSimple.addEventListener("click", function() {
+//     alert("Hola hiciste click en el botón");
+// })
+
+btnSimple.addEventListener("click", () => {
+    alert("Hola hiciste click en el botón");
+});
