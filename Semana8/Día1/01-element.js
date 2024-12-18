59c3aceca06d38bb1c4ba26973ae8b9403e511bc
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-17 19:57:00 -0500

    avance

--- Semana8/Día1/01-element.js
+++ Semana8/Día1/01-element.js
@@ -11,3 +11,20 @@ console.log(h1);
 h1.innerHTML = `
     <code>Noticias Tech Navideñas</code>
 `;
+
+//El <code> previo paso de ser un string a un elemento
+//pero para poder utilizarlo tengo que asegurarme que ESTE EN EL NAVEGADOR para invocarlo
+const codeH1 = document.querySelector("code");
+
+console.log(codeH1);
+
+//En vez de inyectar HTML como texto/string
+//Algo que podemos hacer es crear 01 objeto element de la nada
+//document.createElement me permite crear cualquier elemento
+const imagen = document.createElement("img");
+console.log("imagen", imagen);
+imagen.src = "https://i.pinimg.com/550x/7c/18/65/7c186515514684275ffebddca96dab21.jpg";
+
+const regalosSection = document.querySelector("#regalos");
+
+regalosSection.appendChild(imagen);
