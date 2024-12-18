e924f8172bee606815699bea4b7155d37473a380
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-17 21:07:05 -0500

    avance

--- Semana8/Día1/02-eventos.js
+++ Semana8/Día1/02-eventos.js
@@ -11,3 +11,12 @@ const btnSimple = document.querySelector("#botonSimple");
 btnSimple.addEventListener("click", () => {
     alert("Hola hiciste click en el botón");
 });
+
+const link = document.getElementById("linkAGoogle");
+
+link.addEventListener("click", (evento) => {
+    console.log(evento);
+    //va a prevenir el comportamiento por defecto
+    evento.preventDefault();
+    alert("Diste click a un hipervínculo");
+})
