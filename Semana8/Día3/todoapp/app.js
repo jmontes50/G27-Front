b5c469e7d0e1fa12b67e0fd7e1770b4171d12cb3
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-18 20:58:31 -0500

    avance

--- Semana8/Día3/todoapp/app.js
+++ Semana8/Día3/todoapp/app.js
@@ -16,21 +16,31 @@
 //usualmente cuando trabajamos con el DOM
 //vamos a buscar tener los selectores en la parte superior
 import tareas from "./tareas.js";
+import { ComponenteTarea } from "./componenteTarea.js";
 
 const inputTarea = document.getElementById("inputTarea");
 const btnNuevaTarea = document.querySelector("#btnNuevaTarea");
+const listaTareas = document.querySelector("#listaTareas");
 
 btnNuevaTarea.addEventListener("click", () => {
   //el value es el texto/valor que tiene un input
   const descripcionTarea = inputTarea.value;
   // console.log(descripcionTarea)
   tareas.agregarTarea(descripcionTarea);
-  
+
   dibujarTareas();
 })
 
 const dibujarTareas = () => {
   const listadoTareas = tareas.leerTareas();
   console.table(listadoTareas);
+
+  listaTareas.innerHTML = "";
+
+  listadoTareas.forEach((tarea) => {
+    // console.log("cada tarea", tarea);
+    const elementoTarea = ComponenteTarea(tarea);
+    listaTareas.appendChild(elementoTarea);
+  })
 }
 
--- Semana8/Día3/todoapp/componenteTarea.js
+++ Semana8/Día3/todoapp/componenteTarea.js
@@ -1,8 +1,11 @@
 const ComponenteTarea = (tarea) => {
+  console.log("en componente tarea", tarea);
+  //creamos un elemento div
   const divTarea = document.createElement("div");
+  //lo llenamos de contenido en base a los datos de tarea
   divTarea.innerHTML = `
     <span>${tarea.descripcion}</span>
-    <input type="check" checked=${tarea.finalizado} />
+    <input type="checkbox" class="checkTarea" />
   `;
   //más capaz de lógica
   return divTarea;
