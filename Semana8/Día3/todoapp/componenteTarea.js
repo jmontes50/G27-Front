5cc476fc4cde99ae1c8be1eee898f040e4b1f551
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-18 20:38:28 -0500

    avance

---
+++ Semana8/Día3/todoapp/componenteTarea.js
@@ -0,0 +1,9 @@
+const ComponenteTarea = (tarea) => {
+  const divTarea = document.createElement("div");
+  divTarea.innerHTML = `
+    <span>${tarea.descripcion}</span>
+    <input type="check" checked=${tarea.finalizado} />
+  `;
+  //más capaz de lógica
+  return divTarea;
+}
