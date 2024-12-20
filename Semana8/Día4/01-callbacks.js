ad0525038c3a2d5157c685663fe73404a805c492
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-19 20:39:08 -0500

    avance

--- Semana8/Día4/01-callbacks.js
+++ Semana8/Día4/01-callbacks.js
@@ -20,5 +20,23 @@ const saludar = () => {
     alert("Hola!!!");
 }
 
-btn.addEventListener("click", saludar)
+btn.addEventListener("click", saludar);
+
+//Búsqueda en una base de datos
+//conexión, autenticación, la propia búsqueda
+//DEMORA
+
+const busqueda = (funcionCB) => {
+    //setTimeout va a ejecutar una función después de X Tiempo
+    setTimeout(() => {
+        // console.log("encontrado")
+        // return "data encontrada"
+        funcionCB("el resultado después de buscar");
+    }, 3000)
+}
+
+busqueda((resultado) => {
+    console.log(resultado)
+});
+
 
