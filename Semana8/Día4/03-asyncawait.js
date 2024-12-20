cdc3ee36fbb5da26348b436489098f6917c8da5d
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-19 22:46:43 -0500

    avance

--- Semana8/Día4/03-asyncawait.js
+++ Semana8/Día4/03-asyncawait.js
@@ -10,10 +10,22 @@ const hornear = async () => {
 }
 
 
-hornear()
-.then((res) => {
-    console.log(res)
-})
-.catch((err) => {
-    console.log(err)
-});
+// hornear()
+// .then((res) => {
+//     console.log(res)
+// })
+// .catch((err) => {
+//     console.log(err)
+// });
+
+const proceso = async () => {
+    try {
+        //intenta esto
+        const resultado = await hornear(); //then, resolve,return
+        console.log(resultado);
+    } catch (error) { //catch, reject, throw
+        //pero si encuentro un error lo capturo
+        console.log(error)
+    }
+}
+proceso();
