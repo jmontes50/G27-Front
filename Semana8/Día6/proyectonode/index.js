67b878c470e7232e0ae2eb650c2f51e4db66552f
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-21 09:35:55 -0500

    avance

--- Semana8/Día6/proyectonode/index.js
+++ Semana8/Día6/proyectonode/index.js
@@ -8,7 +8,7 @@ axios.get("https://67661e3d410f84999656dd1e.mockapi.io/productos")
 .then((respuesta) => {
     //en 01 sola respuesta me da tanto la respuesta HTTP con su codigo de estado, el status Text y la data ya lista para utilizar
     //NO ES NECESARIO Hacer un respuesta .json(), no hay necesidad
-    console.log(respuesta);
-    console.log("**********");
+    // console.log(respuesta);
+    console.log("/////////////////////////////////////////");
     console.log(respuesta.data);
 });
