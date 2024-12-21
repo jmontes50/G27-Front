a3dd7cb0de8061728746936148686ea91cb1ee6b
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-21 11:32:35 -0500

    avance

--- Semana8/Día6/01-intro-react/src/App.jsx
+++ Semana8/Día6/01-intro-react/src/App.jsx
@@ -7,6 +7,10 @@ const App = () => {
 
   const titulo = '¡Hola Mundo y React!';
 
+  const saludar = () => {
+    alert('Hola Mundo!!!!');
+  }
+
   //2. tiene que retornar 01 solo elemento, puede ser un componente o HTML
   //lo que se retorne en el componente es lo que se va a mostrar en el navegador
   return (
@@ -14,11 +18,15 @@ const App = () => {
       {/* cada vez que deseemos mezclar o utilizar JS dentro del JSX tenemos que poner { js } */}
       {/* <h1>{titulo}</h1> */}
       {/* Para utilizar componentes lo vamos a poner como una etiqueta html */}
-      <Titulo texto="Holaaaaa mundoooo!" region="Lima" />
+      <Titulo texto={titulo} region="Lima" />
       {/* Titulo(texto), Titulo("Holaaaa") */}
       <Titulo texto="Hello Woooorld!!!" region="Arequipa"/>
       <p>Lorem ipsum dolor sit amet.!!!! {10 + 20} </p>
       {/* es que toda etiqueta o componente tiene que estar cerrado, ya sea con una etiqueta de cierre o un autocierre*/}
+      <button onClick={saludar} >Click!</button>
+      <button onClick={() => alert('Hola Mundo!!!!')} >
+        Click2!
+      </button>
       <hr />
     </div>
   )
