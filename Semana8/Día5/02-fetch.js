6ec9c5787de8749d9561e10af467b3f5caafaa12
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-20 21:07:36 -0500

    avance

--- Semana8/Día5/02-fetch.js
+++ Semana8/Día5/02-fetch.js
@@ -23,9 +23,10 @@ fetch("https://reqres.in/api/users?page=2")
     console.log(err);
 })
 */
+//GET - Obtener
 const obtenerUsuarios = async () => {
     try {
-        const respuesta = await fetch("https://reqres.in/api/users?page=2");
+        const respuesta = await fetch("https://67661e3d410f84999656dd1e.mockapi.io/productos");
         console.log(respuesta);
         if(respuesta.status === 200) {
             const datos = await respuesta.json();
@@ -38,3 +39,30 @@ const obtenerUsuarios = async () => {
     }
 }
 obtenerUsuarios();
+
+//POST - Crear
+const crearProducto = async (nuevoProducto) => {
+    try {
+        //opciones para la petición, POST, PUT, Tenemos que indicarle mediante un objeto la operación a realizar
+        const opciones = {
+            method: "POST", //Método HTTP
+            headers: {
+                "Content-Type": "application/json" //texto, binarios, json, 
+            },
+            body: JSON.stringify(nuevoProducto) //Datos a enviar
+        }
+        const resultado = await fetch("https://67661e3d410f84999656dd1e.mockapi.io/productos", opciones);
+        console.log(resultado)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const prod = {
+    nombre:"Chocolates",
+    descripcion: "Chocolates de caja",
+    precio: 20,
+    imagen:"https://kuyay.pe/wp-content/uploads/2024/05/MG_3131-scaled.jpg"
+}
+crearProducto(prod);
+
