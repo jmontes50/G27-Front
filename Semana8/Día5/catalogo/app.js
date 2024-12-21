1956049bab10bb447e4aaa8a284f7a59a5fc5c8d
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-20 22:12:56 -0500

    avance

--- Semana8/Día5/catalogo/app.js
+++ Semana8/Día5/catalogo/app.js
@@ -7,4 +7,22 @@ const app = async () => {
   const productos = await obtenerProductos();
   console.log(productos);
 }
+
+const divCard = (producto) => {
+  //crear un objeto elemento, un div
+  const div = document.createElement("div");
+  //agregar una clase a un elemento
+  div.classList.add("card");
+  //le da su estructura y concantena con las propiedades de cada producto
+  div.innerHTML = `
+    <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+    <div class="card-body">
+      <h5 class="card-title">${producto.nombre}</h5>
+      <p class="card-text">${producto.descripcion}</p>
+      <p class="card-text">${producto.precio}</p>
+    </div>
+  `;
+  return div;
+}
+
 app();
