c65aadc676d8564c8a859a4977c919c7600b5324
Osmar Montesinos <osmarmontes50@gmail.com>
2024-12-21 11:01:52 -0500

    avance

--- Semana8/Día6/01-intro-react/src/App.jsx
+++ Semana8/Día6/01-intro-react/src/App.jsx
@@ -2,12 +2,17 @@
 //1. si definimos 01 componente su nombre tiene que comenzar con Mayúscula
 const App = () => {
 
+  const titulo = '¡Hola Mundo y React!';
+
   //2. tiene que retornar 01 solo elemento, puede ser un componente o HTML
   //lo que se retorne en el componente es lo que se va a mostrar en el navegador
   return (
     <div>
-      <h1>¡Hola, React!</h1>
-      <p>Lorem ipsum dolor sit amet</p>
+      {/* cada vez que deseemos mezclar o utilizar JS dentro del JSX tenemos que poner { js } */}
+      <h1>{titulo}</h1>
+      <p>Lorem ipsum dolor sit amet.!!!! {10 + 20} </p>
+      {/* es que toda etiqueta o componente tiene que estar cerrado, ya sea con una etiqueta de cierre o un autocierre*/}
+      <hr />
     </div>
   )
 }
