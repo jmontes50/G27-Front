970dfbfee2323112eb415a8f0e524f484e5a9595
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-06 22:05:31 -0500

    avance

--- Semana9/use-effect/src/Formulario.jsx
+++ Semana9/use-effect/src/Formulario.jsx
@@ -1,6 +1,45 @@
+import { useState, useEffect } from "react"
+
 const Formulario = () => {
+  //componentes controlados, tener a un input amarrado a un estado
+  const [nombre, setNombre] = useState("Jorge");
+  const [apellido, setApellido] = useState("Montesinos");
+
+  const manejarNombre = (ev) => {
+    console.log(ev.target.value);
+    setNombre(ev.target.value);
+  }
+
+  //pongamos la lógica
+  //el useEffect se utiliza justo antes del return del componente
+  //cada vez que se actualice se dispara esta función
+  useEffect(() => {
+    console.log("cambiando!")
+  })
+
   return (
-    <div>Formulario</div>
+    // <></> Fragment, no genera ninguna etiqueta pero si sirve para envolver otros elementos/etiquetas
+    <>
+      <form>
+        <label htmlFor="nombre">Nombre:</label>
+        <input
+          id="nombre"
+          type="text"
+          placeholder="Ingrese su nombre"
+          value={nombre}
+          onChange={manejarNombre}
+        />
+        <br />
+        <label htmlFor="apellido">Apellido:</label>
+        <input
+          id="apellido"
+          type="text"
+          placeholder="Ingrese su apellido"
+          value={apellido}
+          onChange={(ev) => {setApellido(ev.target.value)}}
+        />
+      </form>
+    </>
   )
 }
 
