0ad9894cd5e1d6c616ddc15be1130d2b91148683
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-07 20:37:01 -0500

    avance

--- Semana9/Día2/todo/src/components/Task.jsx
+++ Semana9/Día2/todo/src/components/Task.jsx
@@ -1,7 +1,13 @@
 const Task = ({ textoTarea }) => {
   return (
-    <li>{textoTarea}</li>
-  )
-}
+    //react maneja las propiedades de css como un objeto
+    //las 1ras llaves son para indicar que usaremos JS
+    //las 2das es para indicar un objeto
+    <div style={{ display: "flex", gap: "10px", alignItems: "center" }}>
+      <li>{textoTarea}</li>
+      <button>✖️</button>
+    </div>
+  );
+};
 
-export default Task
+export default Task;
