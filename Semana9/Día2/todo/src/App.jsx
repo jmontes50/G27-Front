7268fdf1ed241c86eb2401bc7cdaa3d9b8784924
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-07 20:09:05 -0500

    avance

--- Semana9/Día2/todo/src/App.jsx
+++ Semana9/Día2/todo/src/App.jsx
@@ -1,11 +1,22 @@
 import { useState, useEffect, useRef } from 'react'
 
 const App = () => {
+  const [tareas, setTareas] = useState([]);
+  console.log(tareas)
+
   //es una referencia, pueden pensar en esto como un id que ofrece react para poder acceder a un elemento del DOM
   const nuevaTareaRef = useRef(null);
 
   const manejarClick = () => {
-    console.log(nuevaTareaRef.current.value);
+    // console.log(nuevaTareaRef.current.value);
+    /**1, creamos nuevaTarea que contendra el value del input
+     * 2, mediante un spread operator, hacemos una copia tareas y agregamos nuevaTarea
+     * 3, actualizamos el estado de tareas con la copia que incluye nuevaTarea
+     */
+    const nuevaTarea = nuevaTareaRef.current.value;
+    setTareas([...tareas, nuevaTarea]);
+    //limpiar el input
+    nuevaTareaRef.current.value = '';
   }
 
   return (
