f249a723a4d469edb7e6a1b42d325fa0772f4f57
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-08 20:24:22 -0500

    avance

--- Semana9/Día2/ruteo/src/components/Info.jsx
+++ Semana9/Día2/ruteo/src/components/Info.jsx
@@ -1,7 +1,14 @@
 import { useParams } from "react-router-dom";
 
 const Info = () => {
+  //useParams me permite acceder a los parámetros de la url
+  //si mi ruta configurada es "/info/:cat" y la url es
+  // localhost/info/libros podre recibir el string libros
+  /** como un objeto
+   *  { cat: "libros" }
+   */
   const { cat } = useParams();
+  // console.log(useParams());
   console.log(cat);
 
   const data = {
@@ -13,7 +20,15 @@ const Info = () => {
     ],
   };
 
-  return <div>Info</div>;
+  return (
+    <div>
+      <h1>{cat}</h1>
+      {/* data.electronica data['electronica] */}
+      {data[cat].map((item, i) => (
+        <p key={i}>{item}</p>
+      ))}
+    </div>
+  );
 };
 
 export default Info;
