1d4aedebd78a872c0cb679e348c4907e481aabd4
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-08 19:47:36 -0500

    avance

--- Semana9/Día2/ruteo/src/components/Inicio.jsx
+++ Semana9/Día2/ruteo/src/components/Inicio.jsx
@@ -1,6 +1,14 @@
+//es el equivalente a un hipervinculo <a> de html, solo que para SPA con react router
+import { Link } from "react-router-dom";
+
 const Inicio = () => {
   return (
-    <h1>Inicio</h1>
+    <>
+      <h1>Inicio</h1>
+      <nav>
+        <Link to="/bitacora">Ir a Blog</Link>
+      </nav>
+    </>
   )
 }
 
