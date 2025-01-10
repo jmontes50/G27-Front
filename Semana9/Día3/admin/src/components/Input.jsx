31d3f6871dcbf242436969fc3ce125a0b3d4ce4d
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-09 20:55:37 -0500

    avance

--- Semana9/Día3/admin/src/components/Input.jsx
+++ Semana9/Día3/admin/src/components/Input.jsx
@@ -1,12 +1,12 @@
 //La idea de crear un componente input es que todos los input se parecen, usaremos componentes controlados para asociar el value de cada input a la propiedad de un estado(useState)
-const Input = ({texto, manejarValor}) => {
+const Input = ({estado, texto, manejarValor}) => {
   return (
     <div className="mb-3">
       <label htmlFor={texto}>{texto}</label>
       <input
         type="text"
         placeholder={`Ingrese ${texto}`}
-        value={valor[texto]}
+        value={estado[texto]}
         onChange={manejarValor}
         name={texto}
         id={texto}
