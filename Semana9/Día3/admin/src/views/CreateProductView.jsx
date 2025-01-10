e26216ed80a9fc031175b7922defdea7d86d7a17
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-09 21:06:14 -0500

    avance

--- Semana9/Día3/admin/src/views/CreateProductView.jsx
+++ Semana9/Día3/admin/src/views/CreateProductView.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import Input from "../components/Input";
 
 const CreateProductView = () => {
+  const [producto, setProducto] = useState({
+    nombre:"",
+    descripcion:"",
+    precio:""
+  })
+
+  const manejarInput = (ev) => {
+    setProducto({
+      ...producto,
+      [ev.target.name]: ev.target.value
+    })
+  }
+
+  const inputsACrear = ["nombre", "descripcion", "precio"]
+
   return (
-    <div>CreateProductView</div>
+    <div>
+      <h1>Crear Producto</h1>
+      {inputsACrear.map((item, i) => (
+        <Input 
+          key={i} 
+          estado={producto} 
+          texto={item}
+          manejarValor={manejarInput}
+        />
+      ))}
+    </div>
   )
 }
 
