df0eb81b9b0e4cd0368a335ce065f2724f650241
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-10 19:35:56 -0500

    avance

--- Semana9/Día3/admin/src/App.jsx
+++ Semana9/Día3/admin/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //Vistas a utilizar
 import HomeView from "./views/HomeView";
 import CreateProductView from "./views/CreateProductView"
+import EditProductView from "./views/EditProductView";
 //componentes
 import Navigation from "./components/Navigation";
 
@@ -13,6 +14,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomeView />} />
         <Route path="/createproduct" element={<CreateProductView />} />
+        {/* cuando podemos :referencia esto se recibe en el componente de la ruta */}
+        <Route path="/editproduct/:id" element={<EditProductView />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ Semana9/Día3/admin/src/views/EditProductView.jsx
@@ -0,0 +1,10 @@
+//saber que producto -> obtener la data del producto -> editarla -> mandar la info editada
+import { useState, useEffect } from 'react'
+
+const EditProductView = () => {
+  return (
+    <div>EditProductView</div>
+  )
+}
+
+export default EditProductView
