4a85801a1ce2d883200aff05deacc1927bf06e7d
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-10 20:21:10 -0500

    avance

--- Semana9/Día3/admin/src/views/EditProductView.jsx
+++ Semana9/Día3/admin/src/views/EditProductView.jsx
@@ -1,10 +1,26 @@
 //saber que producto -> obtener la data del producto -> editarla -> mandar la info editada
 import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
+import { requestProductById } from "../services/productService";
 
 const EditProductView = () => {
+  const [producto, setProducto] = useState({
+    id:"",
+    nombre:"",
+    descripcion:"",
+    precio:"",
+    imagen:"",
+  });
+
   const { id } = useParams();
-  
+
+  useEffect(() => {
+    const getProduct = async () => {
+      const productObtained = await requestProductById(id);
+      setProducto(productObtained);
+    }
+    getProduct();
+  }, [])
   return (
     <div>EditProductView</div>
   )
