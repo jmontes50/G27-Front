8da7359830360251528a8e70051a943499eb1ff9
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-14 22:57:42 -0500

    avance

--- Semana10/Día2/ecommerce/src/App.jsx
+++ Semana10/Día2/ecommerce/src/App.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ProductsPage from "./pages/ProductsPage";
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
