356aa727d51837800a21bc2d9ec6938cfb6b7e76
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-14 22:43:47 -0500

    avance

---
+++ Semana10/Día2/ecommerce/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    // vas a buscar compilar todo lo que encuentres dentro de la carpeta src cualquier archivo que tenga la extensión js jsx ts tsx
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
