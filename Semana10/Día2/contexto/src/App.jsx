7b6a3d3816c17fbf686ac185d04c6abcae35ffb6
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-14 20:34:25 -0500

    avance

---
+++ Semana10/Día2/contexto/src/App.jsx
@@ -0,0 +1,13 @@
+import Padre from "./components/Padre"
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <hr/>
+      <Padre />
+    </div>
+  )
+}
+
+export default App
---
+++ Semana10/Día2/contexto/src/components/Padre.jsx
@@ -0,0 +1,11 @@
+
+const Padre = () => {
+  return (
+    <div>
+        <h2>Parent</h2>
+        <hr />
+    </div>
+  )
+}
+
+export default Padre
