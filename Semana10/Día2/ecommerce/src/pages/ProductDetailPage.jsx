f5d1f545cf1459160f7f5646e16437d0151b66a7
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-15 22:46:31 -0500

    avance

--- Semana10/Día2/ecommerce/src/pages/ProductDetailPage.jsx
+++ Semana10/Día2/ecommerce/src/pages/ProductDetailPage.jsx
@@ -9,13 +9,30 @@ const ProductDetailPage = () => {
   const URL = `https://json-server-vercel-git-main-osmar-montesinos-projects.vercel.app/products/${id}`;
   const { data, loading, error } = useGetAxios(URL);
 
+  //no puedo desestructurar el id porque ya declaramos id más arriba
+  const { nombre, descripcion, precio, precio_oferta, imagen, cantidad, estrellas, detalles } = data;
+
   console.log(data)
   if (loading) {
     return <Loading />
   }
 
   return (
-    <div>ProductDetailPage</div>
+    <div className="px-4 py-10 mx-auto lg:px-8 xl:max-w-7xl">
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-2">
+        <div className="object-cover w-full h-full overflow-hidden aspect-auto">
+          <img src={imagen} alt={nombre} className="w-full"/>
+        </div>
+        <div className="mt-2">
+          <h2 className="mb-5 text-3xl font-semibold">{nombre}</h2>
+          <div className="mb-5 text-xl">
+            <span className="me-2">S/ {precio_oferta.toFixed(2)}</span>
+            <span className="line-through text-slate-600">S/ {precio.toFixed(2)}</span>
+          </div>
+          <p className="mb-5">{descripcion}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
