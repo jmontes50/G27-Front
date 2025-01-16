3a706ff44b5045fe09b226a21a21a342ddd0329d
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-15 21:46:21 -0500

    avance

--- Semana10/Día2/ecommerce/src/pages/ProductsPage.jsx
+++ Semana10/Día2/ecommerce/src/pages/ProductsPage.jsx
@@ -1,14 +1,18 @@
 import useGetAxios from "../hooks/useGetAxios";
 import ProductCard from "../components/ui/ProductCard";
+import Loading from "../components/ui/Loading";
 
 const ProductsPage = () => {
   const URL =
     "https://json-server-vercel-git-main-osmar-montesinos-projects.vercel.app/products";
   const { data, loading, error } = useGetAxios(URL);
 
-  console.log("data", data);
-  console.log("loading", loading);
-  console.log("error", error);
+  // console.log("data", data);
+  // console.log("loading", loading);
+  // console.log("error", error);
+  if(loading){
+    return <Loading />
+  }
 
   return (
     <div className="px-4 py-10 mx-auto lg:px-8 xl:max-w-7xl">
