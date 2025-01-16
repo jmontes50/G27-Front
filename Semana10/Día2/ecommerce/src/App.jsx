aaea034c4c1aed207ee568f0f1d5be557c513651
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-15 22:26:16 -0500

    avance

--- Semana10/Día2/ecommerce/src/App.jsx
+++ Semana10/Día2/ecommerce/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
+import ProductDetailPage from "./pages/ProductDetailPage";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/products" element={<ProductsPage />} />
+        <Route path="/product/:id" element={<ProductDetailPage />} />
       </Routes>
     </BrowserRouter>
   )
