26af2526675c2027f0516904b657ebcbb02d7eaf
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-16 20:39:36 -0500

    avance

--- Semana10/Día2/ecommerce/src/components/Product/ProductQuantity.jsx
+++ Semana10/Día2/ecommerce/src/components/Product/ProductQuantity.jsx
@@ -6,7 +6,13 @@ const ProductQuantity = () => {
         <i className="fa-solid fa-minus"></i>
       </button>
       {/* span */}
+      <span className="p-4 border-y-2 border-slate-900">
+        1
+      </span>
       {/* 2do botón */}
+      <button className="p-4 border-y-2 border-e-2 border-slate-900 rounded-e-lg hover:bg-gray-100">
+        <i className="fa-solid fa-plus"></i>
+      </button>
     </div>
   )
 }
