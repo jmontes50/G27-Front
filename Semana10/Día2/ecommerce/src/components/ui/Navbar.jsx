45d13a7945b8eb6aa65e2d80af236bba167f0e44
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-16 22:02:35 -0500

    avance

--- Semana10/Día2/ecommerce/src/components/ui/Navbar.jsx
+++ Semana10/Día2/ecommerce/src/components/ui/Navbar.jsx
@@ -4,7 +4,32 @@ import { Transition } from "@headlessui/react";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='text-black bg-white'>
+      {/* container */}
+      <div className='px-4 mx-auto lg:px-8 xl:max-w-7xl'>
+        <div className='flex justify-between py-8'>
+          {/* logo */}
+          <div className='flex items-center'>
+            Logo
+          </div>
+          {/* links */}
+          <ul className='flex items-center justify-center gap-10'>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Productos</Link>
+            </li>
+          </ul>
+
+          <div className='flex items-center justify-end gap-4'>
+            <Link to="#" className='btn btn-black'>
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
