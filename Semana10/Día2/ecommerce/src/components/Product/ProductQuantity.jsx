650b6c4e56b245661dfe1a15809b13af8e395d23
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-16 20:35:03 -0500

    avance

--- Semana10/Día2/ecommerce/src/components/Product/ProductQuantity.jsx
+++ Semana10/Día2/ecommerce/src/components/Product/ProductQuantity.jsx
@@ -1,8 +1,13 @@
-
-
 const ProductQuantity = () => {
   return (
-    <div>ProductQuantity</div>
+    <div className="flex">
+      {/* 1er botón */}
+      <button className="p-4 border-y-2 border-s-2 border-slate-900 rounded-s-lg hover:bg-gray-100">
+        <i className="fa-solid fa-minus"></i>
+      </button>
+      {/* span */}
+      {/* 2do botón */}
+    </div>
   )
 }
 
