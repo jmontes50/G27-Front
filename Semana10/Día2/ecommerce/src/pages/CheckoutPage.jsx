a24e181ac0411e56af059ac5e983b33cbc390621
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-17 21:56:45 -0500

    avance

--- Semana10/Día2/ecommerce/src/pages/CheckoutPage.jsx
+++ Semana10/Día2/ecommerce/src/pages/CheckoutPage.jsx
@@ -3,17 +3,24 @@ import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 const CheckoutPage = () => {
   return (
     <div className="px-4 py-10 mx-auto lg:px-8 xl:max-w-7xl">
-      <div className="mb-5 text-3xl text-center">Checkout</div>
-      <TabGroup>
-        <TabList>
-          <Tab>Checkout</Tab>
-          <Tab>Datos de envio</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>CONTENIDO CHECKOUT</TabPanel>
-          <TabPanel>CONTENIDO ENVIO</TabPanel>
-        </TabPanels>
-      </TabGroup>
+      <h1 className="mb-5 text-3xl text-center">Checkout</h1>
+      <div className="grid grid-cols-1 gap-10 md:grid-cols-3">
+        <div className="col-span-2">
+          <TabGroup>
+            <TabList>
+              <Tab>Checkout</Tab>
+              <Tab>Datos de envio</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>CONTENIDO CHECKOUT</TabPanel>
+              <TabPanel>CONTENIDO ENVIO</TabPanel>
+            </TabPanels>
+          </TabGroup>
+        </div>
+        <div className="col-span-1">
+          Resumen
+        </div>
+      </div>
     </div>
   );
 };
