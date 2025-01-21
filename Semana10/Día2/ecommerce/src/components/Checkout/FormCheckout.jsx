d5a081129b9efbeebbf9d4f958adc6d858ae37f1
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-20 19:55:06 -0500

    avance

--- Semana10/Día2/ecommerce/src/components/Checkout/FormCheckout.jsx
+++ Semana10/Día2/ecommerce/src/components/Checkout/FormCheckout.jsx
@@ -4,7 +4,15 @@ const FormCheckout = () => {
       <div className="grid grid-cols-1 gap-5 py-4">
         <div className="flex flex-col">
           <label htmlFor="fullname">Nombre Completo:</label>
-          <input type="text" className="form-input"/>
+          <input type="text" className="form-input" id="fullname" />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="address">Dirección:</label>
+          <input type="text" className="form-input" id="address" />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="telephone">Número telefónico:</label>
+          <input type="text" className="form-input" id="telephone" />
         </div>
       </div>
     </form>
