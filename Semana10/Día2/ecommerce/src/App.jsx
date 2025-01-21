44b1ddc78264265c58fbfa0c869378a017022361
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-20 21:56:17 -0500

    avance

--- Semana10/Día2/ecommerce/src/App.jsx
+++ Semana10/Día2/ecommerce/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { CartContextProvider } from "./context/CartContext";
+import { AuthContextProvider } from "./context/AuthContext";
 import { ToastContainer } from "react-toastify";
 import Navbar from "./components/ui/Navbar";
 import HomePage from "./pages/HomePage";
@@ -10,16 +11,18 @@ import CheckoutPage from "./pages/CheckoutPage";
 const App = () => {
   return (
     <BrowserRouter>
-      <CartContextProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/products" element={<ProductsPage />} />
-          <Route path="/product/:id" element={<ProductDetailPage />} />
-          <Route path="checkout" element={<CheckoutPage />} />
-        </Routes>
-        <ToastContainer />
-      </CartContextProvider>
+      <AuthContextProvider>
+        <CartContextProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/products" element={<ProductsPage />} />
+            <Route path="/product/:id" element={<ProductDetailPage />} />
+            <Route path="checkout" element={<CheckoutPage />} />
+          </Routes>
+          <ToastContainer />
+        </CartContextProvider>
+      </AuthContextProvider>
     </BrowserRouter>
   )
 }
