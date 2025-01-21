07385b6c241facc0fd100a40edf079107a33ea5c
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-20 20:11:11 -0500

    avance

--- Semana10/Día2/ecommerce/src/components/Checkout/FormCheckout.jsx
+++ Semana10/Día2/ecommerce/src/components/Checkout/FormCheckout.jsx
@@ -1,18 +1,26 @@
+import { useForm } from "react-hook-form";
+
 const FormCheckout = () => {
+  const { register, handleSubmit, formState: { errors }} = useForm();
+
+  const getDataSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(getDataSubmit)}>
       <div className="grid grid-cols-1 gap-5 py-4">
         <div className="flex flex-col">
           <label htmlFor="fullname">Nombre Completo:</label>
-          <input type="text" className="form-input" id="fullname" />
+          <input type="text" className="form-input" id="fullname" {...register("fullname")} />
         </div>
         <div className="flex flex-col">
           <label htmlFor="address">Dirección:</label>
-          <input type="text" className="form-input" id="address" />
+          <input type="text" className="form-input" id="address" {...register("address")}/>
         </div>
         <div className="flex flex-col">
           <label htmlFor="telephone">Número telefónico:</label>
-          <input type="text" className="form-input" id="telephone" />
+          <input type="text" className="form-input" id="telephone" {...register("telephone")}/>
         </div>
         <button className="btn btn-black" type="submit">
           Validar Datos
