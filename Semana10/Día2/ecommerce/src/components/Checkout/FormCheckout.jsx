7a28b68ae1c0348d4826bd2edce335c75bd9a7f0
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-20 20:28:44 -0500

    avance

--- Semana10/Día2/ecommerce/src/components/Checkout/FormCheckout.jsx
+++ Semana10/Día2/ecommerce/src/components/Checkout/FormCheckout.jsx
@@ -1,26 +1,65 @@
 import { useForm } from "react-hook-form";
 
 const FormCheckout = () => {
-  const { register, handleSubmit, formState: { errors }} = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const getDataSubmit = (data) => {
-    console.log(data)
-  }
+    console.log(data);
+  };
 
   return (
     <form onSubmit={handleSubmit(getDataSubmit)}>
       <div className="grid grid-cols-1 gap-5 py-4">
         <div className="flex flex-col">
           <label htmlFor="fullname">Nombre Completo:</label>
-          <input type="text" className="form-input" id="fullname" {...register("fullname")} />
+          <input
+            type="text"
+            className="form-input"
+            id="fullname"
+            {...register("fullname", { required: true })}
+          />
+          {errors.fullname && (
+            <span className="text-sm text-red-700">
+              Este campo es requerido
+            </span>
+          )}
         </div>
         <div className="flex flex-col">
           <label htmlFor="address">Dirección:</label>
-          <input type="text" className="form-input" id="address" {...register("address")}/>
+          <input
+            type="text"
+            className="form-input"
+            id="address"
+            {...register("address", {
+              required: "Este campo es requerido",
+              minLength: {
+                value: 8,
+                message: "La dirección requiere al menos 8 letras",
+              },
+              maxLength: {
+                value: 25,
+                message: "La dirección requiere ser menor a 26 letras",
+              },
+            })}
+          />
+          {errors.address && (
+            <span className="text-sm text-red-700">
+              {errors.address.message}
+            </span>
+          )}
         </div>
         <div className="flex flex-col">
           <label htmlFor="telephone">Número telefónico:</label>
-          <input type="text" className="form-input" id="telephone" {...register("telephone")}/>
+          <input
+            type="text"
+            className="form-input"
+            id="telephone"
+            {...register("telephone")}
+          />
         </div>
         <button className="btn btn-black" type="submit">
           Validar Datos
