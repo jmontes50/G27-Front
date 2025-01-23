f69c74bdda5bbfeca2ea03507f82282479fee7f9
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-22 22:53:24 -0500

    avance

--- Semana10/Día2/ecommerce/src/components/Home/HeaderHome.jsx
+++ Semana10/Día2/ecommerce/src/components/Home/HeaderHome.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-
 const HeaderHome = () => {
   return (
-    <div>HeaderHome</div>
+    <div className="relative">
+      {/* si queremos tener contenido estáticop (jpg, mp4, etc) en la app lo ideal es ponerlo en public*/}
+      <img src="/public/headerpicture.avif" alt="headerphoto" />
+      <div className="absolute flex flex-col gap-5 top-24 left-20">
+        <span className="text-3xl">
+          Classic Exclusive
+        </span>
+        <h2 className="text-5xl font-bold">
+          Women's Collection
+        </h2>
+        <span className="text-3xl">
+          Up to 40% Off
+        </span>
+        <button className="w-36 btn btn-black">
+          Shop now!!
+        </button>
+      </div>
+    </div>
   )
 }
 
