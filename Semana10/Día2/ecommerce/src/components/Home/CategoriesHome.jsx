fecc3f5a43d40fff4b8873dd02b2dfa7c907a755
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-23 19:27:22 -0500

    avance

--- Semana10/Día2/ecommerce/src/components/Home/CategoriesHome.jsx
+++ Semana10/Día2/ecommerce/src/components/Home/CategoriesHome.jsx
@@ -1,16 +1,25 @@
-import { useState, useEffect } from 'react';
-import useGetAxios from '../../hooks/useGetAxios';
+import useGetAxios from "../../hooks/useGetAxios";
 
 const CategoriesHome = () => {
-  const URL = "https://json-server-vercel-git-main-osmar-montesinos-projects.vercel.app/categories"
+  const URL =
+    "https://json-server-vercel-git-main-osmar-montesinos-projects.vercel.app/categories";
 
   const { data, loading, error } = useGetAxios(URL);
 
-  console.log(data);
-
   return (
-    <div>CategoriesHome</div>
-  )
-}
+    <div>
+      <h2 className="my-10 text-3xl text-left">Shop by Categories</h2>
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-4">
+        {data
+          ? data.map((category) => (
+              <div className="" key={category.id}>
+                {category.nombre}
+              </div>
+            ))
+          : null}
+      </div>
+    </div>
+  );
+};
 
-export default CategoriesHome
+export default CategoriesHome;
