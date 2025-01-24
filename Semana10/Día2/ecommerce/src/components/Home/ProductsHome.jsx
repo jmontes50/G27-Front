f1f6a6df9cd793d41d55c1b6101c0415906d7b3e
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-23 20:02:02 -0500

    avance

--- Semana10/Día2/ecommerce/src/components/Home/ProductsHome.jsx
+++ Semana10/Día2/ecommerce/src/components/Home/ProductsHome.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import useGetAxios from "../../hooks/useGetAxios";
+import ProductCard from "../ui/ProductCard";
 
 const ProductsHome = () => {
-  return (
-    <div>ProductsHome</div>
-  )
-}
+  const URL = `https://json-server-vercel-git-main-osmar-montesinos-projects.vercel.app/products?_limit=4`;
 
-export default ProductsHome
+  const { data, loading, error } = useGetAxios(URL);
+
+  return (
+    <div className="my-10">
+      <h2 className="mb-10 text-3xl">Main Products</h2>
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-4">
+        {data
+          ? data.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))
+          : null}
+      </div>
+    </div>
+  );
+};
+
+export default ProductsHome;
