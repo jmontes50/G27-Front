f2b2c1bc666690022e08f51bc69c6b6ab00f591e
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-23 19:50:14 -0500

    avance

--- Semana10/Día2/ecommerce/src/components/Home/CategoriesHome.jsx
+++ Semana10/Día2/ecommerce/src/components/Home/CategoriesHome.jsx
@@ -12,8 +12,21 @@ const CategoriesHome = () => {
       <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-4">
         {data
           ? data.map((category) => (
-              <div className="" key={category.id}>
-                {category.nombre}
+              // cada tarjeta de categoria
+              <div
+                className="relative overflow-hidden rounded-xl"
+                key={category.id}
+              >
+                <div className="object-cover w-full h-96">
+                  <img
+                    src={category.imagen}
+                    alt={category.nombre}
+                    className="w-full h-full"
+                  />
+                </div>
+                <h3 className="absolute w-10/12 h-10 p-2 font-semibold text-center text-black bg-white rounded left-1/2 bottom-3 transform translate-x-[-50%]">
+                  {category.nombre}
+                </h3>
               </div>
             ))
           : null}
