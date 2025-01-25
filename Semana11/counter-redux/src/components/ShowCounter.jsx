c380cb02822d403dd8c071c31db7e3675049c0cd
Osmar Montesinos <osmarmontes50@gmail.com>
2025-01-24 19:55:32 -0500

    avance

--- Semana11/counter-redux/src/components/ShowCounter.jsx
+++ Semana11/counter-redux/src/components/ShowCounter.jsx
@@ -1,12 +1,22 @@
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux";
+import { increment } from "../app/counter/counterSlice";
 
 const ShowCounter = () => {
   // const estado = useSelector((state) => state);
   // console.log(estado)
   const contador = useSelector((state) => state.counter.value)
 
+  const dispatch = useDispatch();
+
+  const handleIncrement = () => dispatch(increment());
+
   return (
-    <div>ShowCounter {contador}</div>
+    <>
+      <div>ShowCounter: {contador}</div>
+      <button onClick={(handleIncrement)}>
+        Incrementar
+      </button>
+    </>
   )
 }
 
